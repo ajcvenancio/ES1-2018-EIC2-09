3d0de9c0387c8fd5f1f461b86b05f8ed73c0889c
amsgn-iscteiul <amsgn@iscte-iul.pt>
2018-11-15 15:23:47 +0000

    Atualização ao objeto Notification

--- ES1-2018-EIC2-09/src_twitter/twitter/Notification.java
+++ ES1-2018-EIC2-09/src_twitter/twitter/Notification.java
@@ -1,14 +1,32 @@
 package twitter;
 
 public class Notification {
-	String channel,date,source,subject;
-	public Notification(String channel,String date, String source, String subject){
+	private String channel,date,source,subject,text;
+	public Notification(String channel,String date, String source, String subject, String text){
 		this.channel=channel;
 		this.date=date;
 		this.source=source;
 		this.subject=subject;
 	}
+	public String getChannel() {
+		return channel;
+	}
+	public String getDate() {
+		return date;
+	}
+	public String getSource() {
+		return source;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public String getText() {
+		return text;
+	}
 	public String toString(){
 		return " Channel - "+this.channel+"\n Date-"+this.date+"\n Source-"+this.source+"\n Subject-"+this.subject;
 	}
+	
+	
+
 }
