4ed0c531b90050f908a62a425286549e8dc79c90
jhmsa-iscteiul <jhmsa@iscte-iul.pt>
2018-11-17 20:15:02 +0000

    Implementação dos métodos isRegistered que permite verificar se um utlizador já se encontra registado no ficheiro "config.xml" e o método addCredentials que permite adicionar credenciais de um utilizador ao ficheiro "config.xml".

--- src/email.java
+++ src/email.java
@@ -1,3 +1,6 @@
+import java.io.File;
+
+import java.io.IOException;
 import java.util.Properties;
 
 import javax.mail.Folder;
@@ -10,18 +13,56 @@ import javax.mail.Store;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+
+/**
+ * 
+ * @author jhmsa-iscteiul
+ * 
+ *         A seguinte classe é utilizada para enviar do email do utilizador para
+ *         um email a escolha e mostrar a inbox do email do utilizador
+ *
+ */
 
 public class email {
-	
+	private Store store;
+	private static File file = new File("config.xml");
+	private static DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 
 	/**
 	 * *******************************************************
 	 * ***********************SEARCH**************************
 	 * *******************************************************
+	 * 
+	 * Esta função executa a ligação aos servidores do Outlook, indo após a conecção
+	 * buscar todos os emails da pasta Inbox do email do utilizador, colocando-os
+	 * num Array É também invocada uma função que imprime informação dos emails na
+	 * consola
+	 * 
+	 * 
+	 * @param username
+	 *            Este parametro é o endereço de email do utlizador
+	 * @param password
+	 *            Este parametro é a password do endereço de email do utlizador
+	 * @exception NoSuchProviderException
+	 * @exception MessagingException
 	 */
-
-	public void searchEmail(String username, String password) {
+	private void connect(String username, String password) {
 		Properties properties = new Properties();
 
 		properties.put("mail.imap.host", "imap-mail.outlook.com");
@@ -34,10 +75,24 @@ public class email {
 		Session session = Session.getDefaultInstance(properties);
 
 		try {
-
 			/* Connect to the message Store */
-			Store store = session.getStore("imap");
+
+			store = session.getStore("imap");
 			store.connect(username, password);
+			System.out.println("Conectou");
+
+			if (!isRegistered(username, password))
+				addCredentials(username, password);
+
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void searchEmail(String username, String password) {
+
+		try {
 
 			/* open Inbox folder */
 			Folder folderInbox = store.getFolder("INBOX");
@@ -46,28 +101,17 @@ public class email {
 			Message[] foundMessages = folderInbox.getMessages();
 			if (foundMessages.length == 0) {
 				System.out.println("Your Inbox is empty!");
-			}
-			for (int i = foundMessages.length - 1; i >= 0; i--) {
-				Message message = foundMessages[i];
-				System.out.println("From: " + message.getFrom()[0]);
-				System.out.println("Subject: " + message.getSubject());
-				
-
-				System.out.println("Date: " + message.getSentDate());
-
-				System.out.println("");
-				System.out.println("------------: " + i + ":---------------");
-
+			} else {
+				showInbox(foundMessages);
 			}
 
 			folderInbox.close(false);
-			store.close();
+			// store.close();
 
 		} catch (NoSuchProviderException ex) {
 			System.out.println("No provider.");
 			ex.printStackTrace();
 		} catch (MessagingException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
@@ -75,8 +119,58 @@ public class email {
 
 	/**
 	 * *******************************************************
-	 * ***********************SEND**************************
+	 * ***********************SHOW INBOX *********************
 	 * *******************************************************
+	 * 
+	 * A função seguinte imprime na consola O emissor do email, o Assunto e a data
+	 * de envio
+	 *
+	 * @param foundMessages
+	 *            Array com todas as Mensagens da pasta Inbox do utilizador
+	 * @exception MessagingException
+	 *
+	 */
+	private void showInbox(Message[] foundMessages) {
+		for (int i = foundMessages.length - 1; i >= 0; i--) {
+			Message message = foundMessages[i];
+			try {
+				System.out.println("From: " + message.getFrom()[0]);
+				System.out.println("Subject: " + message.getSubject());
+
+				System.out.println("Date: " + message.getSentDate());
+
+				System.out.println("");
+				System.out.println("------------: " + i + ":---------------");
+			} catch (MessagingException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
+	/**
+	 * 
+	 * *******************************************************
+	 * ***********************SEND****************************
+	 * *******************************************************
+	 * 
+	 * Esta função inicialmente executa a conecção com o servidor do Outlook,
+	 * realizando autenticação do Utilizador e posteriormente é criada a mensagem a
+	 * enviar e é feito o seu envio.
+	 * 
+	 * @param receiverEmail
+	 *            Endereço email destino
+	 * @param username
+	 *            Endereço de email do Utilizador
+	 * @param password
+	 *            Password do endereço de email do Utilizador
+	 * @param subject
+	 *            Assunto do email a enviar
+	 * @param msg
+	 *            Corpo da mensagem a enviar no email
+	 * @exception MessagingException
+	 * 
 	 */
 
 	public void send(String receiverEmail, String username, String password, String subject, String msg) {
@@ -84,6 +178,7 @@ public class email {
 		// ************************************
 		// ********** Properties **************
 		// ************************************
+
 		Properties props = new Properties();
 
 		props.put("mail.smtp.host", "smtp-mail.outlook.com");
@@ -120,15 +215,96 @@ public class email {
 		}
 	}
 
+	/**
+	 * 
+	 * Método main,que cria uma instancia da classe email, e faz utilização das suas
+	 * classes send e searchEmail
+	 * 
+	 */
+
+	private void addCredentials(String username, String password) {
+
+		try {
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document xmlDoc = builder.parse(file);
+
+			Element root = xmlDoc.getDocumentElement();
+
+			Element credential = xmlDoc.createElement("credential");
+			credential.setAttribute("Username", username);
+			credential.setAttribute("Password", password);
+			root.appendChild(credential);
+
+			DOMSource source = new DOMSource(xmlDoc);
+
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			StreamResult result = new StreamResult("config.xml");
+			transformer.transform(source, result);
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (TransformerConfigurationException e) {
+			e.printStackTrace();
+		} catch (TransformerException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private boolean isRegistered(String username, String password) {
+
+		try {
+
+			DocumentBuilder builder = factory.newDocumentBuilder();
+
+			Document xmlDoc = builder.parse(file);
+
+			NodeList credentialList = xmlDoc.getElementsByTagName("credential");
+			
+
+			if (credentialList.getLength() == 0)
+				return false;
+
+			for (int i = 0; i < credentialList.getLength(); i++) {
+				NamedNodeMap nodeAttributtes = credentialList.item(i).getAttributes();
+				if (nodeAttributtes.item(1).getNodeValue().equals(username)) {
+					if (nodeAttributtes.item(0).getNodeValue().equals(password)) {
+						return true;
+					}
+				}
+			}
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
 	public static void main(String[] args) {
 
 		String username = "testees@outlook.pt";
 		String password = "TESTEteste123";
+		// Scanner in = new Scanner(System.in);
+		//
+		// System.out.println("Email: ");
+		// String username = in.nextLine();
+		//
+		// System.out.println("Pass: ");
+		// String password = in.nextLine();
+
 		email email = new email();
 
-		email.send(username, username, password, "Subject", "CHEGOU");
-		email.searchEmail(username, password);
-
+		email.connect(username, password);
+		// email.addCredentials(username, password);
+		// email.isRegistred(username, password);
+		System.out.println(email.isRegistered(username, password));
 	}
 
 }
