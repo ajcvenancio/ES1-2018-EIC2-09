915149620996b1faee735b02eefa6938f3cefe7d
amsgn-iscteiul <amsgn@iscte-iul.pt>
2018-11-18 15:36:09 +0000

    Atualização da classe principal do branch Twitter

--- ES1-2018-EIC2-09/src_twitter/twitter/Notification.java
+++ ES1-2018-EIC2-09/src_twitter/twitter/Notification.java
@@ -16,6 +16,9 @@ public class Notification {
 	public String getDate() {
 		return date;
 	}
+	public void setDate(String date){
+		this.date=date;
+	}
 	public String getSource() {
 		return source;
 	}
--- ES1-2018-EIC2-09/src_twitter/twitter/TwitterMain.java
+++ ES1-2018-EIC2-09/src_twitter/twitter/TwitterMain.java
@@ -1,13 +1,15 @@
 package twitter;
 
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
-
 public final class TwitterMain  {
 	//private List<Status> statuses;
 	private TwitterFactory tf;
@@ -44,6 +46,7 @@ public final class TwitterMain  {
 			for(Status s: twitter.getHomeTimeline()){
 				if (s.getUser().getName() != null && s.getUser().getName().contains(user)) {
 					Notification n = new Notification("twitter",s.getCreatedAt().toString(),s.getUser().getName(),"TWITTER NAO TEM SUBJECT",s.getText());
+					n.setDate(changeDateFormat(n.getDate()));
 					notifications.add(n);
 				}
 			}
@@ -54,12 +57,47 @@ public final class TwitterMain  {
 		return notifications;
 	}
 	
+	/*private static String changeDateFormat(){
+		SimpleDateFormat s = new SimpleDateFormat("dd/MMM/yyyy");
+		String dateInString = "Mon OCT 07 19:18:26 CEST 2014";
+		SimpleDateFormat myFormat = new SimpleDateFormat("yyyy-MM-dd");
+		try {
+			Date date = s.parse(
+					dateInString.split(" ")[2] + "/" + dateInString.split(" ")[1] + "/" + dateInString.split(" ")[5]);
+			
+			System.out.println(new SimpleDateFormat("YYYY-MM-dd").format(date));
+			return new SimpleDateFormat("YYYY-MM-dd").format(date);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "Data Nula";
+	}*/
+	
+	private static String changeDateFormat(String dateInString){
+		SimpleDateFormat s = new SimpleDateFormat("dd/MMM/yyyy");
+		//String dateInString = "Mon OCT 07 19:18:26 CEST 2014";
+		SimpleDateFormat myFormat = new SimpleDateFormat("yyyy-MM-dd");
+		String date = dateInString.split(" ")[2] + "-" + dateInString.split(" ")[1] + "-" + dateInString.split(" ")[5] +"  Time: "+ dateInString.split(" ")[3] ;
+		return date;
+		/*
+		 * System.out.println(new SimpleDateFormat("YYYY-MM-dd").format(f));
+		 * return new SimpleDateFormat("YYYY-MM-dd").format(date);
+		 */
+
+	}
+	
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
+		Date d=new Date();
+		d.setDate(18);
+		System.out.println(d);
 		TwitterMain t=new TwitterMain();
 		for(Notification s : t.getStatuses("ISCTE")){
 			System.out.println();
 			System.out.println(s.toString());
 		}
+	
 			
 	}
 }
